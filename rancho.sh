ff9074ffb1509480265a178e7584ac8e20a96f78
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2024-08-14 23:11:30 +0530

    Update rancho.sh

--- rancho.sh
+++ rancho.sh
@@ -4,8 +4,9 @@
 # cat urls.txt | nuclei | ./rancho.sh | notify
 # Prereq - jq, python3, tr
 # File required tldextractor.py file
+# https://gist.githubusercontent.com/mr-rizwan-syed/8933e5183fd8313d24cf854f8a4e9fcd/raw/e02f6bface3485cd5f449a9d6b43e48f4e09a0a7/tldextractor.py
 
-SSLCERTDB="05042024-ssl-cert.json"
+SSLCERTDB="ssl-cert-1008202411PM.json"
 
 # Function to extract IP address from input URL
 extract_ip() {
@@ -19,14 +20,16 @@ while read -r line; do
     ip=$(extract_ip "$line")
 
     # Check if IP is found in the JSON file
-    if grep -q "$ip" $SSLCERTDB; then
+    if rg -q "$ip" $SSLCERTDB; then
+        tlsx1=$(echo $ip | tlsx -cn -silent -ro)
+        httpx1=$(echo $ip | httpx -title -fr -silent| grep -oP '\[\K[^\]]+')
         # Extracting commonNames if IP is found
         commonNames=$(grep "$ip" $SSLCERTDB | jq -r '.commonName' | python3 tldextractor.py)
 
         # Constructing the output format
-        output="$line - [$(echo "$commonNames" | tr '\n' ', ' | sed 's/,$//')]"
+        output="$line - [$(echo "$commonNames" | tr '\n' ', ' | sed 's/,$//')] $tlsx1 $httpx1"
 
         # Echo input and result on the same line
         echo "$output"
     fi
-done 
+done
